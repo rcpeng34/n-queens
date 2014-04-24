c68e6c80a37337db75950a5f814afc7a7ec1777b
Hack Reactor Students <rcpeng@gmail.com>
2014-04-23 22:55:40 -0700

    Implements brute force n-Rooks solution.

--- src/solvers.js
+++ src/solvers.js
@@ -14,22 +14,84 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  // var solution = null; //fixme
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  // // 1. build all possible boards into an array
+  // // 2. check all
+
+
+
+
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  // return solution;
 };
 
 
+window.findCombinations = function(callback, n, k, prev){
+  if(k === undefined){
+    k = n;
+  }
+  if(k === 0){
+    callback(n,prev);
+  } else{
+    var start = prev ? prev[prev.length-1] + 1: 0;
+    for(var i = start; i < n*n; i++){
+      if(!start){
+        prev = [i];
+      } else{
+        prev.push(i);
+      }
+      findCombinations(callback,n,k-1,prev);
+      prev.splice(-1);
+    }
+  }
+};
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var allBoards = [];
+  // build the map
+  var coordinateMap = {};
+  var mapId = 0;
+  for (var row = 0; row < n; row++) {
+    for (var column = 0; column < n; column ++) {
+      coordinateMap[mapId++] = [row, column];
+    }
+  }
 
+  // find all nCk location combinations
+
+  var callback = function(n, coordinatesArray){
+    var coordinates = [];
+    for(var i = 0; i < coordinatesArray.length; i++){
+      coordinates.push(coordinateMap[coordinatesArray[i]]);
+    }
+    var game = window.makeGame(n, coordinates);
+    allBoards.push(game);
+  };
+
+   window.findCombinations(callback,n);
+
+  // loop over all boards and check for solutions
+  for (var i = 0; i < allBoards.length; i++) {
+    if(!allBoards[i].hasAnyRooksConflicts()) { // no conflicts
+      solutionCount++;
+    }
+  }
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
+window.makeGame = function (n, coordinateArray) {
+  var game = new Board({'n': n});
+  for (var coordinate = 0; coordinate < coordinateArray.length; coordinate++) {
+    var row = coordinateArray[coordinate][0];
+    var column = coordinateArray[coordinate][1];
+    game.setPiece(row,column);
+  }
+  return game;
+};
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
