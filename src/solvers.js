5a0c8d0250265b89fd37fc28fb762c3d255b6114
Hack Reactor Students <rcpeng@gmail.com>
2014-04-24 16:58:35 -0700

    Optimizes n-Rooks solution.

--- src/solvers.js
+++ src/solvers.js
@@ -6,93 +6,86 @@
 
 */
 
-// hint: you'll need to do a full-search of all possible arrangements of pieces!
-// (There are also optimizations that will allow you to skip a lot of the dead search space)
-// take a look at solversSpec.js to see what the tests are expecting
-
-
-// return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
-
-window.findNRooksSolution = function(n) {
-  // var solution = null; //fixme
-
-  // // 1. build all possible boards into an array
-  // // 2. check all
 
 
 
 
-  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  // return solution;
-};
 
-// ------------------------------------------------- SUPER IMPORTANT SLEEPY NOTES -----------------------------------------
-// columns, how to prevent findcombination loop from making boards that have queens in the same column
-// store an array = to prev but % each value in prev by n ie newarray[i] = prev[i]%n
-// each time you push start further, check if start%n matches anythign in the new array
-// diagonals, turn array 45 degrees and pretend it's an array
-// diagonals array will have length 2*n
-// for major diagonals, using row-col will give you -n+1 to n-1
-// for minor diagnoals, think about it later
-// find coordinate function, you row and column of a number between 0 and n*n can be mathed to be:
-//    row: math.floor(number/n), col = number%n
 
-window.findCombinations = function(callback, n, k, prev){
-  if(k === undefined){
-    k = n;
+
+
+
+
+
+window.findCombinations = function(callback,n, piecesLeft, prev, occupiedColumns, occupiedMajors, occupiedMinors){
+  if(piecesLeft === 0){
+    // callback(n,prev);
+    window.solutions++;
+  } else if(piecesLeft === undefined){
+    piecesLeft = n;
   }
-  if(k === 0){
-    callback(n,prev);
+  if(prev === undefined){
+    var next = 0;
+    prev = [];
   } else{
-    var start = prev ? prev[prev.length-1] + n - prev[prev.length - 1] % n: 0;
-    for(var i = start; i < n*n; i++){
+    var next = prev[prev.length - 1] + 1;
+  }
+  if(occupiedColumns === undefined){
+    occupiedColumns = {};
+  }
+  // if(occupiedMajors === undefined){
+  //   occupiedMajors = {};
+  // }
+  // if(occupiedMinors === undefined){
+  //   occupiedMinors = {};
+  // }
 
-      if(!start){
-        prev = [i];
-      } else{
-        prev.push(i);
-      }
-      findCombinations(callback,n,k-1,prev);
+  //set next to start at row following last piece addition
+  if(next%n > prev[prev.length-1]%n){
+    next += n - next%n;
+  }
+// var conflictCount = 0;
+  //stops looping if no piece is placed in a row
+  for(var i = 0; i < n; i++){ // don't need i%n as we're starting i at 0 and incrementing to n-1, why i+1<n?
+    // var majorIndex = i - Math.floor((next+i)/n);
+    // var minorIndex = i + Math.floor((next+i)/n);
+    if(!(i in occupiedColumns) /*&& !(majorIndex in occupiedMajors) && !(minorIndex in occupiedMinors)*/){
+      next += i;
+      prev.push(i);
+      occupiedColumns[i] = true;
+      // occupiedMajors[majorIndex] = true;
+      // occupiedMinors[minorIndex] = true;
+      //only recurses once
+      window.findCombinations(callback,n,piecesLeft - 1, prev, occupiedColumns, occupiedMajors, occupiedMinors);
+      //reverses latest changes to prev, occupiedColumns, and next as to not affect next iteration
       prev.splice(-1);
+      delete occupiedColumns[i];
+      next -= i;
     }
+    // else {
+    //   conflictCount++;
+    // }
   }
+  // if (conflictCount === n) {
+  //   console.log("conflictCount = n", n, conflictCount);
+  //   return;
+  // }
+
 };
 
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  var solutionCount = 0;
-  var allBoards = [];
-  // build the map
-  var coordinateMap = {};
-  var mapId = 0;
-  for (var row = 0; row < n; row++) {
-    for (var column = 0; column < n; column ++) {
-      coordinateMap[mapId++] = [row, column];
-    }
-  }
 
-  // find all nCk location combinations
 
-  var callback = function(n, coordinatesArray){
-    var coordinates = [];
-    for(var i = 0; i < coordinatesArray.length; i++){
-      coordinates.push(coordinateMap[coordinatesArray[i]]);
-    }
-    var game = window.makeGame(n, coordinates);
-    allBoards.push(game);
-  };
 
-   window.findCombinations(callback,n);
 
-  // loop over all boards and check for solutions
-  for (var i = 0; i < allBoards.length; i++) {
-    if(!allBoards[i].hasAnyRooksConflicts()) { // no conflicts
-      solutionCount++;
-    }
-  }
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
-};
+
+
+
+
+
+
+
+
+
 
 window.makeGame = function (n, coordinateArray) {
   var game = new Board({'n': n});
@@ -148,4 +141,85 @@ window.countNQueensSolutions = function(n) {
   }
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
+window.countNRooksSolutions = function(n) {
+  var solutions = 0;
+  solutions = window.findRooksCombinations(solutions,n);
+  return solutions;
+};
+
+window.findRooksCombinations = function(solutions, n, piecesLeft, prev, occupiedColumns){
+  if(piecesLeft === 0){
+    solutions++;
+  } else if(piecesLeft === undefined){
+    piecesLeft = n;
+  }
+  if(prev === undefined){
+    var next = 0;
+    prev = [];
+  } else{
+    var next = prev[prev.length - 1] + 1;
+  }
+  if(occupiedColumns === undefined){
+    occupiedColumns = {};
+  }
+
+  //set next to start at row following last piece addition
+  if(next%n > prev[prev.length-1]%n){
+    next += n - next%n;
+  }
+
+  //stops looping if no piece is placed in a row
+  for(var i = 0; i < n; i++){
+    if(!(i in occupiedColumns)){
+      next += i;
+      prev.push(i);
+      occupiedColumns[i] = true;
+      solutions = window.findRooksCombinations(solutions, n,piecesLeft - 1, prev, occupiedColumns);
+      //reverses latest changes to prev, occupiedColumns, and next as to not affect next iteration
+      prev.splice(-1);
+      delete occupiedColumns[i];
+      next -= i;
+    }
+  }
+  return solutions;
 };
